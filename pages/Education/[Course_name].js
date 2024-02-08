77537064d1308b7806327bbc395103b5fc9bd515
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-08 14:12:31 +0530

    Add files via upload

--- pages/Education/[Course_name].js
+++ pages/Education/[Course_name].js
@@ -32,8 +32,95 @@ const DynamicPage = ({ course }) => {
     }, [Course_name]);
     console.log("course name is ")
     console.log("course name is ", course_info);
-    const defaultContent =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const [formErrors, setFormErrors] = useState(null);
+
+    const [successMessage, setSuccessMessage] = useState(null);
+    const setFormDataField = (field, value) => {
+        setFormData({
+            ...formData,
+            [field]: value,
+            'course_name': course_info.course_name,
+            'author_name': course_info.course_author,
+            'payment_link':course_info.paytment_link
+        });
+    };
+    function getCSRFTokenFromCookies() {
+        const csrfCookie = document.cookie
+            .split(';')
+            .map(cookie => cookie.trim())
+            .find(cookie => cookie.startsWith('csrftoken='));
+
+        if (csrfCookie) {
+            return csrfCookie.split('=')[1];
+        }
+
+        return null;
+    }
+
+    const [formData, setFormData] = useState({
+        full_name: '',
+        email_address: '',
+        phone_number: '',
+        city: '',
+        years_of_trading_experience: '',
+
+
+    });
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const fieldName = e.target.name;  // Get the name attribute of the input field
+        const fieldValue = e.target.value;
+
+
+        setFormData({
+            ...formData,
+            [fieldName]: fieldValue,
+        });
+        try {
+            console.log('This is form data -------------');
+            console.log(formData);
+            const response = await fetch('https://trading.work.gd/course_registerations/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': getCSRFTokenFromCookies(),
+                },
+                body: JSON.stringify(formData),
+
+
+            });
+
+            if (response.ok) {
+                console.log('Form submitted successfully!');
+                // Reset form data if needed
+                setFormData({
+                    full_name: '',
+                    email_address: '',
+                    phone_number: '',
+                    city: '',
+                    years_of_trading_experience: '',
+                });
+                setSuccessMessage('Form submitted successfully');
+                
+                window.open(course_info.paytment_link, '_blank');
+
+            } else {
+                const responseData = await response.json();
+                console.error('Form submission failed:', responseData);
+
+                // Check if the response contains field-specific errors
+                if (responseData.errors) {
+                    // Update state to store the errors and display them in your form
+                    setFormErrors(responseData.errors);
+                }
+            }
+        } catch (error) {
+            console.error('An error occurred while submitting the form:', error);
+        }
+    };
 
     return (<>
         {course_info === null ? (
@@ -65,11 +152,13 @@ const DynamicPage = ({ course }) => {
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m18.4 14.8-1.2-1.3a1.7 1.7 0 0 0-2.4 0l-.7.7a1.7 1.7 0 0 1-2.4 0l-1.9-1.9a1.7 1.7 0 0 1 0-2.4l.7-.6a1.7 1.7 0 0 0 0-2.5L9.2 5.6a1.6 1.6 0 0 0-2.4 0c-3.2 3.2-1.7 6.9 1.5 10 3.2 3.3 7 4.8 10.1 1.6a1.6 1.6 0 0 0 0-2.4Z" />
                         </svg>
                         Join Whatsapp Community <br />
-                    {course_info.whatsapp_number}
+                        {course_info.whatsapp_number}
                     </button></Link>
 
                 </div>
-
+            
+              
+              
                 <div className="learnurls">
                     <ul>
                         {course_info.lists.map((list, index) => (
@@ -83,7 +172,87 @@ const DynamicPage = ({ course }) => {
 
                     </ul>
                 </div>
+                <div className="text-center course_register" > <Image src='/course/course_reg.svg' className='text-center webinarregisterimg'
+                    width={500}
+                    height={500}
+                /> </div>
+                    <h5 className='text-white text-center mt-8 webinarregistertext'>Register For the "{course_info.course_name}"  </h5>
 
+                <form className="max-w-md mx-auto webform mb-8" onSubmit={handleSubmit}>
+                    {formErrors && (
+                        <div className="text-red-500">
+                            <ul>
+                                {Object.entries(formErrors).map(([field, errors]) => (
+                                    <li key={field}>{`${field}: ${errors.join(', ')}`}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                    {successMessage &&
+
+
+<span class="badgeclass bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{successMessage}</span>
+}
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-5 group">
+
+
+                            <input type="text"
+                                name="floating_first_name"
+                                id="floating_first_name"
+                                className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                value={formData.full_name}
+                                onChange={(e) => setFormDataField('full_name', e.target.value)}
+                                placeholder=" "
+                                required />
+                            <label for="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Full Name</label>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="email" name="floating_email" id="floating_email" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                value={formData.email_address}
+                                onChange={(e) =>
+                                    setFormDataField('email_address', e.target.value)
+                                }
+                                placeholder=" " required />
+                            <label for="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                        </div>
+                    </div>
+                    <div className="grid md:grid-cols-2 md:gap-6">
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_phone" id="floating_phone" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                value={formData.phone_number}
+                                onChange={(e) =>
+                                    setFormDataField('phone_number', e.target.value)
+                                }
+                                placeholder="" required />
+                            <label for="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Whatsapp Number </label>
+                        </div>
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_company" id="floating_company" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+
+                                value={formData.city}
+                                onChange={(e) =>
+                                    setFormDataField('city', e.target.value)
+                                }
+
+
+                                placeholder=" " required />
+                            <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">City</label>
+                        </div>
+
+
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="number" name="trading_experience" id="trading_experience" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                value={formData.years_of_trading_experience}
+                                onChange={(e) =>
+                                    setFormDataField('years_of_trading_experience', e.target.value)
+                                }
+                                placeholder=" " required />
+                            <label for="trading_experience" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Years of trading experience</label>
+                        </div>
+                    </div>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+                </form>
 
             </div>
             <div className="courses mb-8">
@@ -117,11 +286,11 @@ const DynamicPage = ({ course }) => {
                                 </div>
                             }
                             subtitle={
-                                <div className="text-white"><h5 className="text-white"> {program_details.subtitle}</h5></div>
+                                <div ><h5 className="text-white subtitles"> {program_details.subtitle}</h5></div>
                             }
                             title={
                                 <div className="text-white">
-                                    <h3 className="text-white"> {program_details.title}</h3>
+                                    <h3 className="text-white module_text"> {program_details.title}</h3>
                                 </div>
                             }
 
@@ -129,9 +298,13 @@ const DynamicPage = ({ course }) => {
                         >
                             {<div className="coursesaccordaincontent">
                                 <ul>
-                                    {program_details.description.split("|").map((item, idx) => (
-                                        <li key={idx}> ● {item.trim()}</li>
-                                    ))}
+                                    {program_details.description && program_details.description.length > 0 && (
+                                        <ul>
+                                            {program_details.description.split("|").map((item, idx) => (
+                                                <li key={idx}>● {item.trim()}</li>
+                                            ))}
+                                        </ul>
+                                    )}
 
 
                                 </ul>
@@ -153,30 +326,17 @@ const DynamicPage = ({ course }) => {
 
             </div>
 
-            <div className="courses">
-                <h5 className="text-white mt-5 mb-5 faqsquestion">Course Structure</h5>
-                <div className="accordainclass">
-                    <Accordion variant="bordered">
+            <div className="whatsappcommunitybutton">
 
+<Link href={course_info.whatsapp_link} target='__blank'> <button type="button" className="whatsappbuttoncolor text-white bg-[#3b5998] focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
+    <svg className="w-6 h-6 text-gray-800 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m18.4 14.8-1.2-1.3a1.7 1.7 0 0 0-2.4 0l-.7.7a1.7 1.7 0 0 1-2.4 0l-1.9-1.9a1.7 1.7 0 0 1 0-2.4l.7-.6a1.7 1.7 0 0 0 0-2.5L9.2 5.6a1.6 1.6 0 0 0-2.4 0c-3.2 3.2-1.7 6.9 1.5 10 3.2 3.3 7 4.8 10.1 1.6a1.6 1.6 0 0 0 0-2.4Z" />
+    </svg>
+    Join Whatsapp Community <br />
+    {course_info.whatsapp_number}
+</button></Link>
 
-                        {course_info.course_structures.map((course_structures, ind) => (
-                            <AccordionItem key={ind} aria-label={course_structures.title} title={
-                                <div>
-                                    <p className="text-white">{course_structures.title}</p>
-                                </div>
-                            }>
-
-                                {course_structures.description.split("|").map((item, idx) => (
-                                    <li key={idx}> {item.trim()}</li>
-                                ))}
-
-                            </AccordionItem>
-                        ))}
-
-
-                    </Accordion>
-                </div>
-            </div>
+</div>
 
 
             <div className="courses mt-5 mb-5">
--- pages/Webinar/[webinarName].js
+++ pages/Webinar/[webinarName].js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import React from "react";
 import Base from "../../layouts/Baseof";
+import checkout from '../../layouts/checkout'
 import "swiper/swiper.min.css";
 import Link from "next/link";
 import { useRouter } from 'next/router';
@@ -69,17 +70,20 @@ const WebinarDetail = ({ webinarData }) => {
 
 
                             <div className="flex flex-wrap Webiniarblocks" >
-                                <h3 className="text-white">Who is this Masterclass for:
-                                </h3>
-                                <ul className="learnclass">
-                                    {Webinars.this_class_for && Webinars.this_class_for.map((item, ind) => (
-                                        <li key={ind}>&#9989; {item.content}</li>
-                                    ))}
-                                </ul>
+                                {Webinars.this_class_for && Webinars.this_class_for.length > 0 && (
+                                    <>
+                                        <h3 className="text-white">Who is this Masterclass for:</h3>
+                                        <ul className="learnclass">
+                                            {Webinars.this_class_for.map((item, ind) => (
+                                                <li key={ind}>&#9989; {item.content}</li>
+                                            ))}
+                                        </ul>
+                                    </>
+                                )}
 
 
 
-                                <ul className="classfor mt-5">
+                                {/* <ul className="classfor mt-5">
                                     <li>
                                         <div className="forboxs">
                                             <Image src='/webiniar/graduates.svg'
@@ -107,7 +111,7 @@ const WebinarDetail = ({ webinarData }) => {
                                     </li>
 
 
-                                </ul>
+                                </ul> */}
 
                             </div>
                         </div>
@@ -253,6 +257,7 @@ const WebinarDetail = ({ webinarData }) => {
                 ) :
                     <Base>
                         <div className="flex flex-wrap Webiniarblocks" >
+                            
                             <ServiceCard
                                 title={Webinars.webinar_name}
                                 author_name={Webinars.Author_name}
@@ -270,7 +275,13 @@ const WebinarDetail = ({ webinarData }) => {
 
                         </div>
 
-                        {successMessage &&
+                       
+                        <h5 className='text-white text-center mt-8 webinarregistertext'>Register For the "{Webinars.webinar_name}" Webinar </h5>
+
+                        <Image src='/webiniar/register.svg' className='webinarregisterimg'
+                            width={500}
+                            height={500}
+                        /> {successMessage &&
 
 
                             <span class="badgeclass bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{successMessage}</span>
@@ -316,8 +327,8 @@ const WebinarDetail = ({ webinarData }) => {
                                         onChange={(e) =>
                                             setFormDataField('phone_number', e.target.value)
                                         }
-                                        placeholder=" " required />
-                                    <label for="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+                                        placeholder="" required />
+                                    <label for="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Whatsapp Number </label>
                                 </div>
                                 <div className="relative z-0 w-full mb-5 group">
                                     <input type="text" name="floating_company" id="floating_company" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -329,12 +340,12 @@ const WebinarDetail = ({ webinarData }) => {
 
 
                                         placeholder=" " required />
-                                    <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Country</label>
+                                    <label for="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">City</label>
                                 </div>
 
 
                                 <div className="relative z-0 w-full mb-5 group">
-                                    <input type="text" name="trading_experience" id="trading_experience" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    <input type="number" name="trading_experience" id="trading_experience" className="text-white block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                         value={formData.years_of_trading_experience}
                                         onChange={(e) =>
                                             setFormDataField('years_of_trading_experience', e.target.value)
@@ -355,9 +366,7 @@ const WebinarDetail = ({ webinarData }) => {
                                         {Webinars.webinar_name} by {Webinars.Author_name}
                                     </h3>
                                     <p>
-                                        {Webinars.Webiniar_description_long}   </p>
-
-
+                                        {Webinars.description}   </p>
 
                                 </div>
                             </div>
