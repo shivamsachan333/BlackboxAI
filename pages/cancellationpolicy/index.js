c29ff3627b6d098e138002d9ff62ec9ab29c0abf
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-10 16:50:05 +0530

    Add files via upload

---
+++ pages/cancellationpolicy/index.js
@@ -0,0 +1,65 @@
+// termsandcondition
+
+
+
+import Base from "../../layouts/Baseof";
+
+import "swiper/swiper.min.css";
+
+
+
+
+
+
+const cancellationpolicy = () => {
+
+
+
+    return (
+        <><Base>
+
+
+            <section className="section pb-[50px] " >
+                <div className="container">
+                    <div className="row text-center">
+                        <div className="mx-auto lg:col-10">
+                            <h1 className="font-primary font-bold">
+
+                                BlackBox-AI Cancellation & Refund Policy
+                            </h1>
+
+<h6 className="text-white">SHENSU AI TECHNOLOGIES PRIVATE LIMITED believes in helping its customers as far as possible, and has therefore a liberal cancellation policy. Under this policy:</h6>
+                        </div>
+                    
+                    </div>
+                    <div className="Privacyclass">
+
+                        <p>Cancellations will be considered only if the request is made within 2-3 days of placing the order. However, the cancellation request may not be entertained if the orders have been communicated to the vendors/merchants and they have initiated the process of shipping them.</p>
+                        
+                        
+                        <p>SHENSU AI TECHNOLOGIES PRIVATE LIMITED does not accept cancellation requests for perishable items like flowers, eatables etc. However, refund/replacement can be made if the customer establishes that the quality of product delivered is not good.</p>
+                         
+                         
+                         <p>In case of receipt of damaged or defective items please report the same to our Customer Service team. The request will, however, be entertained once the merchant has checked and determined the same at his own end. This should be reported within 2-3 days of receipt of the products.</p>
+                        
+                        
+                        <p>In case you feel that the product received is not as shown on the site or as per your expectations, you must bring it to the notice of our customer service within 2-3 days of receiving the product. The Customer Service Team after looking into your complaint will take an appropriate decision.</p>
+                      
+                      <p>In case of complaints regarding products that come with a warranty from manufacturers, please refer the issue to them.</p>
+                      <p>In case of any Refunds approved by the SHENSU AI TECHNOLOGIES PRIVATE LIMITED, it’ll take 3-4 days for the refund to be processed to the end customer.</p>
+                      
+                      <p>Disclaimer: The above content is created at SHENSU AI TECHNOLOGIES PRIVATE LIMITED's sole discretion. Razorpay shall not be liable for any content provided here and shall not be responsible for any claims and liability that may arise due to merchant’s non-adherence to it.</p>
+                         </div>
+                </div>
+            </section>
+
+
+
+
+        </Base></>
+    );
+};
+
+
+
+export default cancellationpolicy;
---
+++ pages/shippingdelivery/index.js
@@ -0,0 +1,55 @@
+
+import Base from "../../layouts/Baseof";
+
+import "swiper/swiper.min.css";
+
+
+
+
+
+
+const shippingdelivery = () => {
+
+
+
+    return (
+        <><Base>
+
+
+            <section className="section pb-[50px] " >
+                <div className="container">
+                    <div className="row text-center">
+                        <div className="mx-auto lg:col-10">
+                            <h1 className="font-primary font-bold">
+
+                                BlackBox-AI Shipping & Delivery Policy
+                            </h1>
+
+
+
+                        </div>
+                    
+                    </div>
+                    <div className="Privacyclass">
+                            <p>
+                            For International buyers, orders are shipped and delivered through registered international courier companies and/or International speed post only. For domestic buyers, orders are shipped through registered domestic courier companies and /or speed post only. Orders are shipped within 1-2 days or as per the delivery date agreed at the time of order confirmation and delivering of the shipment subject to Courier Company / post office norms. SHENSU AI TECHNOLOGIES PRIVATE LIMITED is not liable for any delay in delivery by the courier company / postal authorities and only guarantees to hand over the consignment to the courier company or postal authorities within 1-2 days rom the date of the order and payment or as per the delivery date agreed at the time of order confirmation. Delivery of all orders will be to the address provided by the buyer. Delivery of our services will be confirmed on your mail ID as specified during registration. For any issues in utilizing our services you may contact our helpdesk on or
+                            </p>
+                           
+                           
+                        
+                           
+                            
+                          </div>
+                </div>
+            </section>
+
+
+
+
+        </Base></>
+    );
+};
+
+
+
+export default shippingdelivery;
