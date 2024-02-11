13666b0fce216f36796b82dcaeec3831118c00cc
Shivam Sachan <43928746+shivamsachan333@users.noreply.github.com>
2024-02-11 10:12:22 +0530

    Update homestrategy.js

--- layouts/components/services/homestrategy.js
+++ layouts/components/services/homestrategy.js
@@ -75,38 +75,7 @@ function Homestrategy() {
               <h2 className="text-white">Something You Need To Know</h2>
             </div>
             <div className="mt-8 grid gap-x-8 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
-              {strategy.slice(0, 5).map((item, i) => (
-                <Link
-                  href={{
-                    pathname: '/[StrategyName]', query: {
-                      strategyname: item.Strategy_name,
-                      strategydescription: item.description,
-                      visual1: item.visual1,
-                      visual2: item.visual2,
-                      visual3: item.visual3,
-                      visual4: item.visual4
-                    }
-                  }}
-                  as={`/${item.Strategy_name.replace(/\s/g, '')}`}
-                  rel=''
-                >
-                  <div
-                    className="feature-card rounded-xl p-5 pb-8 text-center"
-                    key={`feature-${i}`}
-                  >
-                    {item.IconsImage && (
-                      <Image
-                        className="mx-auto"
-                        src={item.IconsImage}
-                        width={50}
-                        height={50}
-                        alt="" />
-                    )}
-                    <div className="mt-4">
-                      <h5 className="text-white" ><b>{item.Strategy_name}</b></h5>
-                      <p className="mt-3 text-white">{truncateDescription(item.description, 100)} Read more</p>
-                    </div>
-                  </div></Link>
+             
               ))}
 
               <Link href="/Services/Strategy Trading/">
